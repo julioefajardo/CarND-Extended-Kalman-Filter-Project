7f8fbd6a22e9eea4d85e7eca0013c11cb8d216cb
Julio Fajardo <julioefajaro@gmail.com>
2017-04-24 11:47:32 -0600

    initial commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,74 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd y = z - H_ * x_; 
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;					// Residual Covariance
+  MatrixXd Si = S.inverse();	
+  MatrixXd PHt = P_ * Ht;				
+  MatrixXd K = PHt * Si; 						// Kalman Gain
+
+  // new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  float rho = sqrt(x_[0] * x_[0] + x_[1] * x_[1]);
+  float phi = atan2(x_[1], x_[0]);
+  float rho_dot = 0; 
+  if (fabs(rho) > 0.0001) rho_dot = (x_[0] * x_[2] + x_[1] * x_[3]) / rho; 
+  
+  VectorXd h(3);
+  h << rho, phi, rho_dot;
+
+  VectorXd y = z - h; 
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;					// Residual Covariance
+  MatrixXd Si = S.inverse();	
+  MatrixXd PHt = P_ * Ht;				
+  MatrixXd K = PHt * Si; 						// Kalman Gain
+ 
+  // new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
---
+++ src/tools.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if ((ground_truth.size()!=estimations.size())||(estimations.size()==0)){
+    std::cout << "Invalida Data!" << std::endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd residual = estimations[i] - ground_truth[i];
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse/estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+
+  MatrixXd Hj(3,4);
+  //recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  //pre-compute a set of terms to avoid repeated calculation
+  float norm = sqrt(px*px+py*py);
+  float norm2 = px*px+py*py;
+  
+  //check division by zero
+  if(fabs(norm2) < 0.0001){
+    std::cout << "CalculateJacobian () - Error - Division by Zero" << std::endl;
+    return Hj;
+  }
+
+  //compute the Jacobian matrix
+  Hj << px/norm, py/norm, 0 , 0,
+       -py/norm2, px/norm2, 0 , 0,
+        py*(vx*py - vy*px)/(norm*norm2), px*(px*vy - py*vx)/(norm*norm2), px/norm, py/norm;
+  
+  return Hj;
+}
